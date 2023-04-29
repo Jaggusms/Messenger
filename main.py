6efe9f5137bcfe9ed3f946b11de1caa1c6c9a888
jagadeesh <sanniboinajagadeesh@gmail.com>
2023-04-29 23:12:26 +0530

    data base

--- main.py
+++ main.py
@@ -17,7 +17,11 @@ add_bg_from_local('source/charcoal.png')
 
 with open("css/style.css") as f:
         st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
-st.write("Please wait data loading!")
+from datetime import datetime
+
+
+st.write("Please wait loading!")
+
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
@@ -25,25 +29,19 @@ uri = "mongodb+srv://Jaggusmk:4321@cluster0.bzwyzf9.mongodb.net/?retryWrites=tru
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
-
 data=list(client.newdatabase.messges.find())[::-1]
-for i in  data:
-     if i.get("message","")!="":
-         st.write(str(i.get("message","")))
-
-
-
-from datetime import datetime
 d=dict()
+out=0
 message="Hi Welcome"
-d['Time'],d['message']=datetime.now(),message
 with st.form("form 1",clear_on_submit=True):
     message=st.text_area(label="Enter New Message", placeholder="Message")
     out=st.form_submit_button("Submit")
     if out:  
         d['Time'],d['message']=datetime.now(),message
-        st.success("Submit Successfully, Please Refresh the page")
-        client['newdatabase']['messges'].insert_one(d)      
-# from pymongo import MongoClient
-# client = MongoClient("mongodb+srv://Jaggusmk:4321@cluster0.bzwyzf9.mongodb.net/?retryWrites=true&w=majority",ssl=True)
+        client['newdatabase']['messges'].insert_one(d)
+        st.success("Submit Successfully, Please refresh the page")
+for i in  data:
+     if i.get("message","")!="":
+         st.write(str(i.get("message","")))
 
+ 
